9cca266a01eb1ff01db324cb9962430b43eaa130
ricardo <ricardoyepezz@gmail.com>
2024-05-05 18:10:29 -0400

    fix: linter fixes

--- src/app/components/Camera.js
+++ src/app/components/Camera.js
@@ -2,61 +2,61 @@
 import React, { useRef, useEffect } from 'react';
 
 const Camera = ({ onPhotoUploaded }) => {
-    const videoRef = useRef(null);
-    const canvasRef = useRef(null);
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
 
-    useEffect(() => {
-        const constraints = { video: { facingMode: { ideal: 'environment' } } };
-        const getVideo = async () => {
-            try {
-                const stream = await navigator.mediaDevices.getUserMedia(constraints);
-                if (videoRef.current) videoRef.current.srcObject = stream;
-            } catch (error) {
-                console.error('Error accessing the camera:', error);
-            }
-        };
-        getVideo();
-    }, []);
-
-    const takePhoto = () => {
-        const width = videoRef.current.videoWidth;
-        const height = videoRef.current.videoHeight;
-        canvasRef.current.width = width;
-        canvasRef.current.height = height;
-        const ctx = canvasRef.current.getContext('2d');
-        ctx.drawImage(videoRef.current, 0, 0, width, height);
-
-        canvasRef.current.toBlob(blob => {
-            uploadFile(blob);
-        }, 'image/jpeg');
+  useEffect(() => {
+    const constraints = { video: { facingMode: { ideal: 'environment' } } };
+    const getVideo = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        if (videoRef.current) videoRef.current.srcObject = stream;
+      } catch (error) {
+        console.error('Error accessing the camera:', error);
+      }
     };
+    getVideo();
+  }, []);
 
-    const uploadFile = (file) => {
-        const formData = new FormData();
-        formData.append('file', file);
+  const takePhoto = () => {
+    const width = videoRef.current.videoWidth;
+    const height = videoRef.current.videoHeight;
+    canvasRef.current.width = width;
+    canvasRef.current.height = height;
+    const ctx = canvasRef.current.getContext('2d');
+    ctx.drawImage(videoRef.current, 0, 0, width, height);
 
-        fetch('https://upload-expenses-app.rj.r.appspot.com/upload', {
-            method: 'POST',
-            body: formData,
-        })
-        .then(response => {
-          return response.json(); // Asegúrate de procesar la respuesta como JSON
-      })        .then(data => {
-            console.log(`File uploaded to ${data.url}`);
-            onPhotoUploaded(data.url); // Pasar la URL de la imagen al formulario
-        })
-        .catch(err => {
-            console.error('Error uploading file:', err);
-        });
-    };
+    canvasRef.current.toBlob(blob => {
+      uploadFile(blob);
+    }, 'image/jpeg');
+  };
 
-    return (
-        <div>
-            <video ref={videoRef} autoPlay playsInline />
-            <button onClick={takePhoto}>Take Photo</button>
-            <canvas ref={canvasRef} style={{ display: 'none' }}></canvas>
-        </div>
-    );
+  const uploadFile = (file) => {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    fetch('https://upload-expenses-app.rj.r.appspot.com/upload', {
+      method: 'POST',
+      body: formData,
+    })
+      .then(response => {
+        return response.json(); // Asegúrate de procesar la respuesta como JSON
+      }).then(data => {
+        console.log(`File uploaded to ${data.url}`);
+        onPhotoUploaded(data.url); // Pasar la URL de la imagen al formulario
+      })
+      .catch(err => {
+        console.error('Error uploading file:', err);
+      });
+  };
+
+  return (
+    <div>
+      <video ref={videoRef} autoPlay playsInline />
+      <button onClick={takePhoto}>Take Photo</button>
+      <canvas ref={canvasRef} style={{ display: 'none' }}></canvas>
+    </div>
+  );
 };
 
 export default Camera;
