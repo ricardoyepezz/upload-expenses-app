4c4f27487d1063c6190f605a54200348119f0d82
ricardo <ricardoyepezz@gmail.com>
2024-05-04 20:08:37 -0400

    feat: storage configurations and implementation added

---
+++ src/api/googleStorage.js
@@ -0,0 +1,15 @@
+import { Storage } from '@google-cloud/storage';
+
+const credentials = JSON.parse(process.env.REACT_APP_GOOGLE_CREDENTIALS_JSON);
+
+const storage = new Storage({
+  projectId: credentials.project_id,
+  credentials: {
+    client_email: credentials.client_email,
+    private_key: credentials.private_key
+  }
+});
+
+const bucketName = 'upload-expenses-app-bucket-1';
+
+export default { storage, bucketName}
--- src/app/components/Camera.js
+++ src/app/components/Camera.js
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect } from 'react';
-
+import { storage, bucketName } from '../../api/googleStorage';
+  
 const Camera = () => {
     const videoRef = useRef(null);
 
@@ -24,7 +25,46 @@ const Camera = () => {
         getVideo();
     }, []);
 
-    return <video ref={videoRef} autoPlay playsInline />;
+    const takePhoto = () => {
+        const width = videoRef.current.videoWidth;
+        const height = videoRef.current.videoHeight;
+        canvasRef.current.width = width;
+        canvasRef.current.height = height;
+
+        const ctx = canvasRef.current.getContext('2d');
+        ctx.drawImage(videoRef.current, 0, 0, width, height);
+
+        canvasRef.current.toBlob(blob => {
+            uploadFile(blob, storage, bucketName);
+        }, 'image/jpeg');
+    };
+
+    const uploadFile = (file, storage, bucketName) => {
+        const bucket = storage.bucket(bucketName);
+        const blob = bucket.file(`photo-${Date.now()}.jpg`);
+        const blobStream = blob.createWriteStream({
+          resumable: true,
+          metadata: {
+            contentType: 'image/jpeg',
+          },
+        });
+  
+        blobStream.on('error', err => console.error('Error uploading file:', err));
+        blobStream.on('finish', () => {
+          const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
+          console.log(`File uploaded to ${publicUrl}`);
+        });
+  
+        blobStream.end(file);
+      };
+
+      return (
+        <div>
+            <video ref={videoRef} autoPlay playsInline />
+            <button onClick={takePhoto}>Take Photo</button>
+            <canvas ref={canvasRef} style={{ display: 'none' }}></canvas>
+        </div>
+    );
 };
 
 export default Camera;
