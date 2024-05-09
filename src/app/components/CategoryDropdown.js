470f34cbca346d10d2f6c62e3883275c005fc9dc
ricardo <ricardoyepezz@gmail.com>
2024-05-08 20:23:19 -0400

    fix: categories values updated

--- src/app/components/CategoryDropdown.js
+++ src/app/components/CategoryDropdown.js
@@ -6,10 +6,10 @@ const CategoryDropdown = ({ value, onChange }) => {
       <label>Category:</label>
       <select value={value} onChange={onChange}>
         <option value="">Select a category</option>
-        <option value="food">Adriana travel expenses</option>
-        <option value="travel">AQC travel expenses</option>
-        <option value="other">Operation logistic and EPP</option>
-        <option value="other">Other (machine, spare parts, etc)</option>
+        <option value="Adriana travel expenses">Adriana travel expenses</option>
+        <option value="AQC travel expenses">AQC travel expenses</option>
+        <option value="Operation logistic and EPP">Operation logistic and EPP</option>
+        <option value="Other (machine, spare parts, etc">Other (machine, spare parts, etc)</option>
       </select>
     </div>
   );
